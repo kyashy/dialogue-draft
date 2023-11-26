e2c4dd3c97c0839c3dcc57e2c5c1f312dc93a324
kyashy <kyashy4321@gmail.com>
2023-11-26 12:06:37 +0900

    Hide advanced settings with accordion

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ def create_dataframe(result):
 
 
 def transcribe_audio(
-        input_file,
+        audio_path,
         model_name,
         language,
         initial_prompt,
         progress=gr.Progress(track_tqdm=True)
     ):
+    if audio_path is None:
+        raise gr.Error("Please upload your audio file.")
     if language == "Auto":
         language = None
     print(f"Loading whisper Model...")
@@ -27,7 +29,7 @@ def transcribe_audio(
     model.to("cuda:0")
     print(f"Generating Transcript...")
     result = model.transcribe(
-        audio=input_file,
+        audio=audio_path,
         language=language,
         fp16=False,
         verbose=True,
@@ -35,37 +37,52 @@ def transcribe_audio(
     )
 
     df = create_dataframe(result)
-    output_file = "data/output/" + input_file.split("/")[-1].split(".")[0] + ".csv"
+    output_file = "data/output/" + audio_path.split("/")[-1].split(".")[0] + ".csv"
     df.to_csv(output_file, index=False, encoding="utf-8-sig")
 
     return df, output_file
 
 
 def main():
+    with gr.Blocks() as demo:
+        # Title of the app
+        gr.Markdown("# DialogueDraft")
+        with gr.Row():
+            # Define inputs
+            with gr.Column():
+                audio_path = gr.Audio(
+                    sources=["upload"],
+                    type="filepath",
+                    label="Audio File"
+                )
+                print(audio_path)
+                submit_button = gr.Button("Submit")
+                with gr.Accordion(label="Advanced Settings", open=False):
+                    model_name = gr.Dropdown(
+                        choices=["tiny", "base", "small", "medium", "large"],
+                        value="large",
+                        label="Model"
+                    )
+                    language = gr.Dropdown(
+                        choices=["Japanese", "English", "Auto"],
+                        value="Japanese",
+                        label="Language"
+                    )
+                    initial_prompt = gr.Textbox(
+                        value="議事録を作成します。",
+                        label="Initial Prompt"
+                    )
+            # Define outputs
+            with gr.Column():
+                result = gr.Dataframe(label="Result", headers=["start", "end", "text"])
+                download = gr.File(label="Download")
 
-    demo = gr.Interface(
-        fn=transcribe_audio,
-        inputs=[
-            gr.Audio(sources=["upload"], type="filepath", label="Audio File"),
-            gr.Dropdown(
-                choices=["tiny", "base", "small", "medium", "large"],
-                value="large",
-                label="Model",
-            ),
-            gr.Dropdown(
-                choices=["Japanese", "English", "Auto"],
-                value="Japanese",
-                label="Language",
-            ),
-            gr.Textbox(value="議事録を作成します。", label="Initial Prompt"),
-        ],
-        outputs=[
-            gr.Dataframe(label="Result", headers=["start", "end", "text"]),
-            gr.File(label="Download"),
-        ],
-        title="DialogueDraft",
-        allow_flagging="never",
-    )
+        submit_button.click(
+            transcribe_audio,
+            inputs=[audio_path, model_name, language, initial_prompt],
+            outputs=[result, download]
+        )
+
     # Queue functionality for handling multiple users
     demo.queue()
     demo.launch()
