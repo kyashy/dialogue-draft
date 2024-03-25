397dede0869dade7ff51d4adbad8042a03dbc809
kyashy <kyashy4321@gmail.com>
2024-03-26 08:04:04 +0900

    Free up memory to avoid CUDA out of memory error

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import gradio as gr
 import pandas as pd
+import torch
 import whisper
 from whisper.utils import format_timestamp
 
@@ -20,24 +21,33 @@ def transcribe_audio(
     initial_prompt,
     progress=gr.Progress(track_tqdm=True),
 ):
-    print(f"Loading whisper Model...")
-    # If you specify device="cuda:0", 8GB memory will not be enough, so first put it on the CPU.
-    model = whisper.load_model(name=model_name, device="cpu", download_root=".cache")
-    # Then move only the model to GPU.
-    model.to("cuda:0")
-
     if language == "Auto":
+        # Load the language detection model
+        print(f"Detecting language...")
+        lang_detection_model = whisper.load_model(
+            name="base", device="cuda:0", download_root=".cache"
+        )
         # load audio and pad/trim it to fit 30 seconds
         audio = whisper.load_audio(audio_path)
         audio = whisper.pad_or_trim(audio)
 
         # make log-Mel spectrogram and move to the same device as the model
-        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+        mel = whisper.log_mel_spectrogram(audio).to(lang_detection_model.device)
 
         # detect the spoken language
-        _, probs = model.detect_language(mel)
+        _, probs = lang_detection_model.detect_language(mel)
         language = max(probs, key=probs.get)
-        gr.Info(f"Detected language: {language}")
+        print(f"Detected language: {language}")
+
+        # Free up memory to avoid CUDA out of memory error
+        del lang_detection_model
+        torch.cuda.empty_cache()
+
+    print(f"Loading whisper Model...")
+    # If you specify device="cuda:0", 8GB memory will not be enough, so first put it on the CPU.
+    model = whisper.load_model(name=model_name, device="cpu", download_root=".cache")
+    # Then move only the model to GPU.
+    model.to("cuda:0")
 
     print(f"Generating Transcript...")
     result = model.transcribe(
@@ -55,6 +65,10 @@ def transcribe_audio(
 
     output_dir.mkdir(parents=True, exist_ok=True)
     df.to_csv(output_file, index=False, encoding="utf-8-sig")
+
+    # Free up memory to avoid CUDA out of memory error
+    del model
+    torch.cuda.empty_cache()
 
     return df, str(output_file)
 
